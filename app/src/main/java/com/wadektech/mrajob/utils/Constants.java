718958c0b8da7085d2772affce868b0337136dfc
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-10-20 17:18:06 +0300

    implemented map activity in home fragment

--- app/src/main/java/com/wadektech/mrajob/utils/Constants.java
+++ app/src/main/java/com/wadektech/mrajob/utils/Constants.java
@@ -1,5 +1,8 @@
 package com.wadektech.mrajob.utils;
 
+import com.wadektech.mrajob.models.JobSeeker;
+
 public class Constants {
   public static final String JOB_SEEKER_INFO_REFERENCE = "JobSeekerInfo";
+  public static JobSeeker currentUser;
 }
