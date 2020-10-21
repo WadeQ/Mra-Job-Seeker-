671cff19f516e5e9918cd11c0b69fe39df5ba385
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-10-21 19:50:44 +0300

    changes to job seeker profile

--- app/src/main/java/com/wadektech/mrajob/ui/JobSeekerHomeActivity.java
+++ app/src/main/java/com/wadektech/mrajob/ui/JobSeekerHomeActivity.java
@@ -1,22 +1,38 @@
 package com.wadektech.mrajob.ui;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.wadektech.mrajob.R;
 import com.wadektech.mrajob.utils.Constants;
+import com.wadektech.utils.JobSeekerUtils;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -26,12 +42,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JobSeekerHomeActivity extends AppCompatActivity {
 
+  private static final int IMAGE_PICKER_REQUEST_CODE = 1988;
   private AppBarConfiguration mAppBarConfiguration;
   DrawerLayout drawer;
   NavigationView navigationView;
   NavController navController;
+  AlertDialog alertDialog;
+  StorageReference storageReference ;
+  Uri imageUri;
+  ImageView userProfileImage ;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -51,37 +75,125 @@ public class JobSeekerHomeActivity extends AppCompatActivity {
     NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
     NavigationUI.setupWithNavController(navigationView, navController);
 
-    navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-      @Override
-      public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.nav_sign_out) {
-          AlertDialog.Builder builder = new AlertDialog.Builder(JobSeekerHomeActivity.this);
-          builder.setTitle("Sign Out")
-              .setMessage("Are you sure you want to sign out?")
-              .setNegativeButton("CANCEL", (dialog, which) -> dialog.dismiss())
-              .setPositiveButton("SIGN OUT", (dialog, which) -> JobSeekerHomeActivity.this.signOutUser())
-              .setCancelable(false);
-          AlertDialog dialog = builder.create();
-          dialog.setOnShowListener(dialog1 -> {
-            dialog.getButton(AlertDialog.BUTTON_POSITIVE)
-                .setTextColor(getResources().getColor(android.R.color.holo_red_dark));
-            dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
-                .setTextColor(getResources().getColor(R.color.colorAccent));
-          });
-          dialog.show();
-        }
-        return true;
+    initParameters();
+  }
+
+  private void initParameters() {
+    alertDialog = new AlertDialog.Builder(this)
+        .setCancelable(false)
+        .setMessage("Uploading...")
+        .create();
+
+    storageReference = FirebaseStorage.getInstance().getReference();
+    navigationView.setNavigationItemSelectedListener(item -> {
+      if (item.getItemId() == R.id.nav_sign_out) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(JobSeekerHomeActivity.this);
+        builder.setTitle("Sign Out")
+            .setMessage("Are you sure you want to sign out?")
+            .setNegativeButton("CANCEL", (dialog, which) -> dialog.dismiss())
+            .setPositiveButton("SIGN OUT", (dialog, which) -> JobSeekerHomeActivity.this.signOutUser())
+            .setCancelable(false);
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(dialog1 -> {
+          dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+              .setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+          dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+              .setTextColor(getResources().getColor(R.color.colorAccent));
+        });
+        dialog.show();
       }
+      return true;
     });
 
     View view = navigationView.getHeaderView(0);
     TextView username = view.findViewById(R.id.tv_user_name);
     TextView userPhone = view.findViewById(R.id.tv_user_phone_number);
     TextView rating = view.findViewById(R.id.tv_user_rating);
+    userProfileImage = view.findViewById(R.id.user_image_profile);
+
+    userProfileImage.setOnClickListener(v -> {
+      Intent intent = new Intent();
+      intent.setType("image/*");
+      intent.setAction(Intent.ACTION_GET_CONTENT);
+      startActivityForResult(intent, IMAGE_PICKER_REQUEST_CODE);
+    });
 
     username.setText(Constants.userWelcomeBanner());
     userPhone.setText(Constants.currentUser != null ? Constants.currentUser.getPhoneNumber(): "");
     rating.setText(Constants.currentUser != null ? String.valueOf(Constants.currentUser.getRating()): "0.0");
+    if (Constants.currentUser != null && Constants.currentUser.getImageUrl() != null &&
+                  !TextUtils.isEmpty(Constants.currentUser.getImageUrl())){
+      Glide.with(this)
+          .load(Constants.currentUser.getImageUrl())
+          .into(userProfileImage);
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == IMAGE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+      if (data != null && data.getData() != null){
+        imageUri = data.getData();
+        userProfileImage.setImageURI(imageUri);
+        showUploadProgress();
+      }
+    }
+  }
+
+  private void showUploadProgress() {
+      AlertDialog.Builder builder = new AlertDialog.Builder(JobSeekerHomeActivity.this);
+      builder.setTitle("Change Profile Image")
+          .setMessage("Are you sure you want to change image?")
+          .setNegativeButton("CANCEL", (dialog, which) -> dialog.dismiss())
+          .setPositiveButton("UPLOAD", (dialog, which) -> {
+            if (imageUri != null){
+              alertDialog.setMessage("Upload started...");
+              alertDialog.show();
+              String key = FirebaseAuth.getInstance().getCurrentUser().getUid();
+              StorageReference rootRef = storageReference.child("profileImages/"+key);
+              rootRef.putFile(imageUri)
+                  .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                      alertDialog.dismiss();
+                      Snackbar.make(drawer, "Upload error "
+                          + e.getMessage(), Snackbar.LENGTH_LONG).show();
+                    }
+                  })
+                  .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                      if (task.isSuccessful()) {
+                        rootRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                          @Override
+                          public void onSuccess(Uri uri) {
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("profileImages", uri.toString());
+                            JobSeekerUtils.updateJobSeekerCredentials(drawer, map);
+                          }
+                        });
+                      }
+                      alertDialog.dismiss();
+                    }
+                  }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                  double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                  alertDialog.setMessage(new StringBuilder("Uploading: ").append(progress).append("%"));
+                }
+              });
+            }
+          })
+          .setCancelable(false);
+      AlertDialog dialog = builder.create();
+      dialog.setOnShowListener(dialog1 -> {
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            .setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+            .setTextColor(getResources().getColor(R.color.colorAccent));
+      });
+      dialog.show();
   }
 
   private void signOutUser() {
