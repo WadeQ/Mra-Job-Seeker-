9f32d789d4cb268e71df6f743011e8ede35e3605
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-10-21 11:39:23 +0300

    implemented user online status listener

--- app/src/main/java/com/wadektech/mrajob/utils/Constants.java
+++ app/src/main/java/com/wadektech/mrajob/utils/Constants.java
@@ -4,5 +4,6 @@ import com.wadektech.mrajob.models.JobSeeker;
 
 public class Constants {
   public static final String JOB_SEEKER_INFO_REFERENCE = "JobSeekerInfo";
+  public static final String JOB_SEEKER_LOCATION_REFERENCE = "JobSeekerLocation" ;
   public static JobSeeker currentUser;
 }
