486c9e89700ef1f6084033ca92c9b48787b60400
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-10-22 12:43:24 +0300

    implemented firebase FCM service

--- app/src/main/java/com/wadektech/utils/JobSeekerUtils.java
+++ app/src/main/java/com/wadektech/utils/JobSeekerUtils.java
@@ -1,16 +1,16 @@
 package com.wadektech.utils;
 
+import android.content.Context;
 import android.view.View;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
+import android.widget.Toast;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.wadektech.mrajob.models.Token;
 import com.wadektech.mrajob.utils.Constants;
 import java.util.Map;
+
 
 public class JobSeekerUtils {
   public static void updateJobSeekerCredentials(View view, Map<String, Object> map){
@@ -19,17 +19,26 @@ public class JobSeekerUtils {
         .getReference(Constants.JOB_SEEKER_INFO_REFERENCE)
         .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
         .updateChildren(map)
-        .addOnFailureListener(new OnFailureListener() {
-          @Override
-          public void onFailure(@NonNull Exception e) {
-            Snackbar.make(view, "Error updating profile!" + e.getMessage(), Snackbar.LENGTH_LONG).show();
-          }
-        })
-        .addOnSuccessListener(new OnSuccessListener<Void>() {
-          @Override
-          public void onSuccess(Void aVoid) {
-            Snackbar.make(view, "Profile updated..", Snackbar.LENGTH_LONG).show();
-          }
-        });
+        .addOnFailureListener(e -> Snackbar.make(view, "Error updating profile!" + e.getMessage(),
+            Snackbar.LENGTH_LONG).show())
+        .addOnSuccessListener(aVoid -> Snackbar.make(view, "Profile updated successfully..",
+            Snackbar.LENGTH_LONG).show());
+  }
+
+  public static void updateToken(Context context, String token) {
+    Token userToken = new Token();
+    FirebaseDatabase
+        .getInstance()
+        .getReference(Constants.TOKEN_REFERENCE)
+        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+        .setValue(userToken)
+        .addOnFailureListener(e ->
+            Toast.makeText(context, "Error saving token "+e.getMessage(),Toast.LENGTH_SHORT)
+                .show()).addOnSuccessListener(new OnSuccessListener<Void>() {
+      @Override
+      public void onSuccess(Void aVoid) {
+
+      }
+    });
   }
 }
